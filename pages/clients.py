99bc5795e3b27e0f2eb1fb0d68c60b26c81b344d
Ilyacine <Ilyacine@Tp1_git.com>
2025-10-21 14:44:38 +0100

    fix

--- pages/clients.py
+++ pages/clients.py
@@ -1082,7 +1082,7 @@ if st.session_state.aff_content == True:
 
             # Espacement et affichage
             plt.tight_layout()
-            st.pyplot(fig)
+            st.pyplot(fig, width="stretch")
     if "process_done" in st.session_state and st.session_state.process_done == False:
         # 1️⃣ Récupérer les données du client sélectionné
         client_data = df.iloc[[client_index]]  # on garde la forme DataFrame
